d3720f3623bde566ccca78bfdcc0b4ce9f8b11f2
Serj7k <165706142+Serj7k@users.noreply.github.com>
2026-01-29 11:11:41 +0300

    Update PYTHON_PASSWORD_.py

--- PYTHON_PASSWORD_.py
+++ PYTHON_PASSWORD_.py
@@ -1,15 +1,16 @@
 # <yes> <report> PYTHON_PASSWORD_EMPTY 607f01
+password = ""
+password = "123"  
 password = "123444"
-password = ""  
-password = "123"
 # <yes> <report> PYTHON_PASSWORD_NULL c37c10
 password = None
 # <yes> <report> PYTHON_PASSWORD_HARDCODED eed164
-password = "not_so_hardcoded"
+password = "hardcoded"
 # <yes> <report> PYTHON_PASSWORD_HARDCODED eed164
-psswd = "hardcoded"
+psswd = "also_hardcoded"
 
-pwd = "also_hardcoded"
+pwd = "not_so_hardcoded"
 # <no> <report>
 
 password = pwd
+
